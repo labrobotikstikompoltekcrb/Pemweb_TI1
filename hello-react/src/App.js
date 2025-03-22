39c4471ad83cf4c25eac82e3e68b35c2410ed811
labrobotikstikompoltekcrb <labrobotikstikom19@gmail.com>
2025-03-22 10:20:56 +0700

    Pertemuan 2

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+// Import Libraries
+import React from "react";
+import Header from "./Header.jsx";
+// Membuat komponen dengan class
+
+// Create Component
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>Nama saya Firdaus</p>
+      <Header />
+      </div >
+    
+
+  );
+};
+// Export Default
+export default App;
---
+++ hello-react/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Header extends React.Component {
+    render() {
+      return (
+        <div> 
+          <h2>Makanan Khas Indonesia (Header Terpisah File)</h2>
+        </div>
+      );
+    }
+  } 
+  export default Header
